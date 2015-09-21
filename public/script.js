8ceab7e5008b6b1740044244e36ab819312732dc
github username <github email address>
2015-09-21 16:40:59 -0400

    added intro

--- public/script.js
+++ public/script.js
@@ -1,4 +1,26 @@
-console.log("scrtip loinked")
-// $("#info").click(function(e){
-//
-// })
+$(document).ready(function(){
+
+    console.log("scrtip loinked")
+    var content = ["Hi", "Try a keys from left to right, A to K","OoOoO sounds...","OOooOo colors...","OoOoOO shapes...", "Now try a chord", "Click that danged 'o' button and go crazy"]
+    var msg = 0
+    $("#wrapper")[0].textContent = content[msg]
+
+    $("#next").click(function(e){
+      if(msg < content.length - 1){
+
+        msg += 1
+        $("#wrapper")[0].textContent = content[msg]
+        console.log("works?")
+      }
+    })
+
+    $("#prev").click(function(e){
+      if(msg > 0){
+
+        msg -= 1
+        $("#wrapper")[0].textContent = content[msg]
+        console.log("works?")
+      }
+    })
+
+})
