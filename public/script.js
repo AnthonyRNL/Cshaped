98bfff49e4a8d16c6bf56cf3c8e61b5d757c175b
github username <github email address>
2015-09-25 21:07:01 -0400

    changed modal to popup on load

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-
     console.log("scrtip loinked")
-    var content = ["Hi", "There are sounds, colors and shapes here", "Try pressing a key from left to right, A to K","OoOoO sounds...","OOooOo colors...","OoOoOO shapes...", "Look around for the sharps and flats","Now try a C major chord", "*hint* 'A', 'D', 'G' or 'A', 'D', 'G', 'K' *hint*", "Click that danged 'o' button to your right and go crazy"]
+    var content = ["Hi", "Sounds, colors and shapes here", "All within a chromatic octave beginning in C", "Try pressing a key from left to right, A to K","OoOoO sounds...","OOooOo colors...","OoOoOO shapes...", "Look around for the sharps and flats","Now try a C major chord", "*hint* 'A', 'D', 'G' or 'A', 'D', 'G', 'K' *hint*", "Click '>'' and go crazy with different chords!"]
     var msg = 0
     $("#wrapper")[0].textContent = content[msg]
 
@@ -11,6 +10,8 @@ $(document).ready(function(){
         msg += 1
         $("#wrapper")[0].textContent = content[msg]
         console.log("works?")
+      }  else if(msg === content.length - 1){
+        $("#container").hide()
       }
     })
 
@@ -22,5 +23,19 @@ $(document).ready(function(){
         console.log("works?")
       }
     })
+    // $("body").click(function(e){
+    //   $("#lookie").hide()
+    // })
+    $("#info").click(function(e){
+      msg = 0
+      $("#wrapper")[0].textContent = content[msg]
+      $("#container").show()
+    })
+    function show_popup()
+    {
+      document.getElementById('container').style.display = 'block';
+    }
+
+    window.onload = $("#container").show();
 
 })
